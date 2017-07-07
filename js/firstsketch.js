5d813664da4f2873f44ff42ce5712b3374d031bc
cfosco <camilofosco@gmail.com>
2017-07-07 04:59:25 -0300

    defining main doodle

--- js/firstsketch.js
+++ js/firstsketch.js
@@ -1,18 +1,137 @@
 window.onload = function setup() {
-
     var canvas = createCanvas(window.innerWidth, window.innerHeight);
     canvas.parent("sketch-holder");
 
+      background(0);
 
+      stroke(255, 255, 255, 100);
+
+      // start the x and y position at lower-left corner
+      x = 0;
+      y = height-1;
+
+      // COMPUTE THE L-SYSTEM
+      for (var i = 0; i < numloops; i++) {
+        thestring = lindenmayer(thestring);
+      }
+}
+
+// function draw() {
+//
+//   if(mouseX != previousMouseX || mouseY != previousMouseY) {
+//
+//     line(mouseX, mouseY, mouseX+30, mouseY);
+//     previousMouseX = mouseX;
+//     previousMouseY = mouseY;
+//
+//   }
+//
+//   // var c = color(mouseX, mouseY, 1);
+//   // fill(c);
+//   // rect(0,0,width,height);
+//
+// }
+
+window.onresize = function() {
+var w = window.innerWidth;
+var h = window.innerHeight;
+resizeCanvas(w,h);
+width = w;
+height = h;
+
+      background(0);
+};
+
+
+/* Global variables */
+previousMouseX = 0;
+previousMouseY = 0;
+
+
+// TURTLE STUFF:
+var x, y; // the current position of the turtle
+var currentangle = 0; // which way the turtle is pointing
+var step = 20; // how much the turtle moves with each 'F'
+var angle = 90; // how much the turtle turns with a '-' or '+'
+
+// LINDENMAYER STUFF (L-SYSTEMS)
+var thestring = 'A'; // "axiom" or start of the string
+var numloops = 5; // how many iterations to pre-compute
+var therules = []; // array for rules
+therules[0] = ['A', '-BF+AFA+FB-']; // first rule
+therules[1] = ['B', '+AF-BFB-FA+']; // second rule
+
+var whereinstring = 0; // where in the L-system are we?
+
+
+function draw() {
+
+    if (mouseIsPressed) {
+      step = mouseX % 100;
+    }
+  // draw the current character in the string:
+  drawIt(thestring[whereinstring]);
+
+  // increment the point for where we're reading the string.
+  // wrap around at the end.
+  whereinstring++;
+  if (whereinstring > thestring.length-1) whereinstring = 0;
 
 }
 
-function draw() {
-  background(0);
-  stroke(100);
-  for (var i = 0; i < 200; i += 20) {
-    bezier(mouseX-(i/2.0), 40+i, mouseY-410, 20, 440, 300, 240-(i/16.0), 300+(i/8.0));
+// interpret an L-system
+function lindenmayer(s) {
+  var outputstring = ''; // start a blank output string
 
+  // iterate through 'therules' looking for symbol matches:
+  for (var i = 0; i < s.length; i++) {
+    var ismatch = 0; // by default, no match
+    for (var j = 0; j < therules.length; j++) {
+      if (s[i] == therules[j][0])  {
+        outputstring += therules[j][1]; // write substitution
+        ismatch = 1; // we have a match, so don't copy over symbol
+        break; // get outta this for() loop
+      }
+    }
+    // if nothing matches, just copy the symbol over.
+    if (ismatch == 0) outputstring+= s[i];
   }
 
+  return outputstring; // send out the modified string
+}
+
+// this is a custom function that draws turtle commands
+function drawIt(k) {
+
+  if (k=='F') { // draw forward
+    // polar to cartesian based on step and currentangle:
+    var x1 = x + step*cos(radians(currentangle));
+    var y1 = y + step*sin(radians(currentangle));
+    line(x, y, x1, y1); // connect the old and the new
+
+    // update the turtle's position:
+    x = x1;
+    y = y1;
+  } else if (k == '+') {
+    currentangle += angle; // turn left
+  } else if (k == '-') {
+    currentangle -= angle; // turn right
+  }
+
+  // give me some random color values:
+  var r = random(128, 255);
+  var g = random(0, 192);
+  var b = random(0, 50);
+  var a = random(50, 100);
+
+  // pick a gaussian (D&D) distribution for the radius:
+  var radius = 0;
+  radius += random(0, 15);
+  radius += random(0, 15);
+  radius += random(0, 15);
+  radius = radius/3;
+
+  // draw the stuff:
+  fill(r, g, b, a);
+  ellipse(x, y, radius, radius);
 }
